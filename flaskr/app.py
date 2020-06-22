409cc9decd4ad46d3b4917c5e788072c4801c8e8
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2020-06-22 10:39:11 +0200

    Added README and updated environment variables Changes to execution schema

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,10 +1,10 @@
-import os
 from flask import Flask
 from flask_restful import Api
 from flaskr.endpoints.instance import InstanceEndpoint
 from flaskr.endpoints.user import UserEndpoint
 from flaskr.endpoints.login import LoginEndpoint
 from flaskr.endpoints.execution import ExecutionEndpoint
+from flaskr.endpoints.singup import SingUpEndpoint
 from flaskr.config import app_config
 from flaskr.models import db, bcrypt
 
@@ -22,10 +22,11 @@ def create_app(env_name):
     bcrypt.init_app(app)
     db.init_app(app)
     api = Api(app)
-    api.add_resource(InstanceEndpoint, '/instance')
-    api.add_resource(ExecutionEndpoint, '/execution')
-    api.add_resource(UserEndpoint, '/user')
-    api.add_resource(LoginEndpoint, '/login')
+    api.add_resource(InstanceEndpoint, '/instance/')
+    api.add_resource(ExecutionEndpoint, '/execution/')
+    api.add_resource(UserEndpoint, '/user/')
+    api.add_resource(LoginEndpoint, '/login/')
+    api.add_resource(SingUpEndpoint, '/singup/')
 
     return app
 
--- flaskr/config.py
+++ flaskr/config.py
@@ -7,10 +7,10 @@ class Development(object):
     """
     DEBUG = True
     TESTING = True
-    # SECRET_KEY = os.getenv('SECRET_KEY')
-    SECRET_KEY = "THISNEEDSTOBECHANGED"
-    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    SQLALCHEMY_DATABASE_URI = "postgres://postgres:postgresadmin@127.0.0.1:5432/cornflow"
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    # SECRET_KEY = "THISNEEDSTOBECHANGED"
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    # SQLALCHEMY_DATABASE_URI = "postgres://postgres:postgresadmin@127.0.0.1:5432/cornflow"
 
 
 class Production(object):
