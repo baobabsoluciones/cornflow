421fa46383490e073642092e0ff8a82b51da76db
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2020-06-22 10:39:11 +0200

    Added README and updated environment variables Changes to execution schema

--- flaskr/schemas/execution_schema.py
+++ flaskr/schemas/execution_schema.py
@@ -1,13 +1,19 @@
 from marshmallow import fields, Schema
 
+class OptionsSchema(Schema):
+    option1 = fields.Int(required=False)
+
 class ConfigSchema(Schema):
+    solver = fields.Str(default="PULP_CBC_CMD")
     mip = fields.Boolean(required=True)
+    msg = fields.Boolean(required=True)
+    warmStart = fields.Boolean(required=True)
     timeLimit = fields.Integer(required=True)
-    solver = fields.Str(required = True)
-    mipStart = fields.Boolean(required=True)
-    cores = fields.Integer(required=True)
-    maxNodes= fields.Integer(required=True)
-    maxMemory = fields.Integer(required=True)
+    options = fields.Nested(OptionsSchema, required=True)
+    keepFiles = fields.Boolean(required=True)
     gapRel = fields.Float(required=True)
     gapAbs = fields.Float(required=True)
-    keepFiles = fields.Boolean(required=True)
+    maxMemory = fields.Integer(required=True)
+    maxNodes = fields.Integer(required=True)
+    threads = fields.Integer(required=True)
+    logPath = fields.Str(required=True)
--- flaskr/shared/resource.py
+++ flaskr/shared/resource.py
@@ -2,4 +2,5 @@ from flask_restful import Resource
 from flaskr.shared.authentication import Auth
 
 class BaseResource(Resource):
-    method_decorators = [Auth.auth_required]
+    # method_decorators = [Auth.auth_required]
+    pass
