6535498ac059ea1d2ef6e5254f7de7488dd3fe3e
Hugo Larzabal <Hugo.Larzabal@baobabsoluciones.es>
2020-07-07 12:43:28 +0200

    funciones de python para usar la api. cambios en endpoint y instance.py a limpiar

---
+++ api_functions.py
@@ -0,0 +1,41 @@
+import requests
+
+
+def sign_up(email, pwd, name):
+    return requests.post(
+        "http://127.0.0.1:5000/singup/",
+        json={"email": email, "password": pwd, "name": name})
+
+
+def login(email, pwd):
+    response = requests.post(
+        "http://127.0.0.1:5000/login/",
+        json={"email": email, "password": pwd})
+    
+    return response.json()["token"]
+
+
+def create_instance(token, data):
+    response = requests.post(
+        "http://127.0.0.1:5000/instance/",
+        headers={'Authorization': 'access_token ' + token},
+        json={"data": data})
+    
+    return response.json()["instance_id"]
+
+
+def create_execution(token, instance_id, config):
+    response = requests.post(
+        "http://127.0.0.1:5000/execution/",
+        headers={'Authorization': 'access_token ' + token},
+        json={"config": config, "instance": instance_id})
+    return response.json()["execution_id"]
+
+
+def get_data(token, execution_id):
+    response = requests.get(
+        "http://127.0.0.1:5000/execution/",
+        headers={'Authorization': 'access_token ' + token},
+        json={"execution_id": execution_id})
+    
+    return response.json()
--- flaskr/endpoints/execution.py
+++ flaskr/endpoints/execution.py
@@ -1,8 +1,8 @@
 from flask import request
 from flask_restful import Resource
 
-from flaskr.models.execution import ExecutionModel, ExecutionSchema
-from flaskr.models.instance import InstanceModel
+from flaskr.models.execution import ExecutionModel
+from flaskr.models.instance import InstanceModel, ExecutionSchema
 from flaskr.shared.authentication import Auth
 
 execution_schema = ExecutionSchema()
@@ -24,11 +24,17 @@ class ExecutionEndpoint(Resource):
 
         return {'execution_id': ser_data.get('reference_id')}, 201
 
+    # @Auth.auth_required
+    # def get(self):
+    #     user_id = Auth.return_user(request)
+    #     executions = ExecutionModel.get_all_executions_user(user_id)
+    #     ser_executions = execution_schema.dump(executions, many=True)
+    #
+    #     return ser_executions, 200
+    
     @Auth.auth_required
     def get(self):
-        user_id = Auth.return_user(request)
-        executions = ExecutionModel.get_all_executions_user(user_id)
-        ser_executions = execution_schema.dump(executions, many=True)
-
-        return ser_executions, 200
+        req_data = request.get_json()
+        execution_data = ExecutionModel.get_execution_data(req_data["execution_id"])
+        return execution_data, 200
 
