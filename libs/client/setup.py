fb200ed353930947c5b5188914bb1e88e5ef040a
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2020-09-30 13:16:58 +0200

    Version 0.0.1 of cornflow-client

---
+++ libs/client/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cornflow-client",
+    version="0.0.1",
+    author="baobab soluciones",
+    author_email="sistemas@baobabsoluciones.es",
+    description="Client to connect to a cornflow server",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/baobabsoluciones/cornflow-client",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha"
+    ],
+    python_requires='>=3.6',
+    install_requires=['requests', 'urllib']
+)
---
+++ src/__init__.py
@@ -0,0 +1,1 @@
+from src.cornflow_client import CornFlow
