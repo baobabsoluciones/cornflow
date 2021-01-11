a48f91b373ad5318ddc6c9d99f98e5abc8a20be4
pchtsp <pchtsp@gmail.com>
2021-01-11 11:59:18 +0100

    added CORS in development

--- cornflow/app.py
+++ cornflow/app.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 
 from .config import app_config
 from .endpoints import InstanceEndpoint, InstanceDetailsEndpoint, UserEndpoint, LoginEndpoint, \
@@ -19,7 +20,8 @@ def create_app(env_name='development'):
 
     app = Flask(__name__)
     app.config.from_object(app_config[env_name])
-
+    if env_name=='development':
+        CORS(app)
     bcrypt.init_app(app)
     db.init_app(app)
     api = Api(app)
