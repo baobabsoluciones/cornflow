35004bc970c7d485d30a0c2f2800ff81bdcf3552
pchtsp <pchtsp@gmail.com>
2021-01-14 21:37:38 +0100

    airflow integration for detailed execution status

--- cornflow/shared/airflow_api.py
+++ cornflow/shared/airflow_api.py
@@ -14,27 +14,37 @@ class Airflow(object):
         self.auth = HTTPBasicAuth(user, pwd)
 
     def is_alive(self):
-        response = requests.get(self.url)
-        # TODO: complete this!
-        return True
+        response = requests.get(self.url + '/health')
+        data = response.json()
+        return data['metadatabase']['status']=='healthy' and \
+               data['scheduler']['status']=='healthy'
 
-    def run_dag(self, execution_id, cornflow_url):
-        conf = dict(exec_id=execution_id,
-                    cornflow_url=cornflow_url)
-        # Content-type: application/json
-        # Accept: application/json
-        # headers = {'Authorization': 'access_token ' + self.token},
-        response = requests.post(
-            urljoin(self.url, '/api/v1/dags/solve_model_dag/dagRuns'),
+    def consume_dag_run(self, dag_name, payload, dag_run_id=None, method='POST'):
+        url = urljoin(self.url, '/api/v1/dags/{}/dagRuns'.format(dag_name))
+        if dag_run_id is not None:
+            url = url + '/{}'.format(dag_run_id)
+        response = requests.request(
+            method=method,
+            url=url,
             headers={'Content-type': 'application/json',
                      'Accept': 'application/json'},
             auth=self.auth,
-            json={"conf": conf})
+            json=payload)
         if response.status_code != 200:
             raise AirflowApiError('Airflow responded with a status: {}:\n{}'.
                                   format(response.status_code, response.text))
-        return True
+        return response
 
+    def run_dag(self, execution_id, cornflow_url, dag_name='solve_model_dag'):
+        conf = dict(exec_id=execution_id, cornflow_url=cornflow_url)
+        payload = dict(conf=conf)
+        return self.consume_dag_run(dag_name, payload=payload, method='POST')
+
+    def get_dag_run_status(self, dag_name, dag_run_id):
+        return self.consume_dag_run(dag_name, payload=None, dag_run_id=dag_run_id, method='GET')
+
+    def get_all_dag_runs(self, dag_name):
+        return self.consume_dag_run(dag_name=dag_name, payload=None, method='GET')
 
 
 class AirflowApiError(Exception):
