9a3c7888a044e9159ab54efdabbebc5dd7b658e4
pchtsp <pchtsp@gmail.com>
2021-01-18 17:21:04 +0100

    added new tests to cornflow client. The execution test (commented) fails.

--- cornflow/tests/unit/test_cornflowclient.py
+++ cornflow/tests/unit/test_cornflowclient.py
@@ -9,10 +9,10 @@ class TestInstances(CustomTestCaseLive):
     #  delete instance, new / delete execution, user management
     #  get status?
 
-    def create_new_instance_file(self, file):
+    def create_new_instance_file(self, mps_file):
         name = 'test_instance1'
         description = 'description123'
-        response = self.client.create_instance_file(filename=file,
+        response = self.client.create_instance_file(filename=mps_file,
                                                        name=name,
                                                        description=description)
         self.assertTrue('id' in response)
@@ -20,9 +20,46 @@ class TestInstances(CustomTestCaseLive):
         self.assertEqual(row.id, response['id'])
         self.assertEqual(row.name, name)
         self.assertEqual(row.description, description)
-        payload = pulp.LpProblem.fromMPS(file)[1].toDict()
+        payload = pulp.LpProblem.fromMPS(mps_file)[1].toDict()
         self.assertEqual(row.data, payload)
+        return row
 
-    def test_new_instance(self):
+    def create_new_instance(self, mps_file):
+        name = 'test_instance1'
+        description = 'description123'
+        data = pulp.LpProblem.fromMPS(mps_file)[1].toDict()
+        response = self.client.create_instance(data=data,
+                                               name=name,
+                                               description=description)
+        self.assertTrue('id' in response)
+        row = InstanceModel.query.get(response['id'])
+        self.assertEqual(row.id, response['id'])
+        self.assertEqual(row.name, name)
+        self.assertEqual(row.description, description)
+        payload = pulp.LpProblem.fromMPS(mps_file)[1].toDict()
+        self.assertEqual(row.data, payload)
+        return row
+
+    def create_new_execution(self, instance_id, config):
+        name = "test_execution_name_123"
+        description = 'test_execution_description_123'
+        response = self.client.create_execution(instance_id=instance_id,
+                                     config=config,
+                                     description=description,
+                                     name=name)
+        self.assertTrue('id' in response)
+        row = InstanceModel.query.get(response['id'])
+        self.assertEqual(row.id, response['id'])
+        self.assertEqual(row.name, name)
+        self.assertEqual(row.description, description)
+        return row
+
+    def test_new_instance_file(self):
         self.create_new_instance_file('./cornflow/tests/data/test_mps.mps')
 
+    def test_new_instance(self):
+        self.create_new_instance('./cornflow/tests/data/test_mps.mps')
+
+    # def test_new_execution(self):
+    #     one_instance = self.create_new_instance('./cornflow/tests/data/test_mps.mps')
+    #     self.create_new_execution(one_instance.id, config=dict(solver='PULP_CBC_CMD', timeLimit=10))
