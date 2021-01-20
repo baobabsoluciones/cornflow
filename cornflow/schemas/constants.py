ec941b62f30bd88a33c266abb7a28156ce277584
hugolarzabal <40272209+hugolarzabal@users.noreply.github.com>
2021-01-20 18:17:03 +0100

    Feature/json schema (#17)
    * json schema for pulp model
    
    class to manage json and marshmallow schema (in progress)
    
    * class SchemaManager works and can convert a jsonschema to a dict.
    
    work in progress:
    - complete SchemaManager to transform into flask schemas.
    - add test
    - add more general cases (more types...)
    
    * schema manager works.
    
    Instance and execution class need to be tested
    
    * airflow integration for detailed execution status
    
    * added instance-file endpoint and tests. Also refactored some of the tests and chose sqlite for tests.
    
    * moved files so they match the new organization.
    
    * changes after testing the cornflow api.
    
    * added new tests to cornflow client. The execution test (commented) fails.
    
    * refactored old schema tests to fit inside unittests. Also: added github actions.
    
    * first, let's make it work with ubuntu.
    
    * corrections according to PR comments (first part)
    
    * add tests for the schemaManager
    
    * changes in validation function
    
    * correction in test_schemas
    
    * DATASCHEMA !!!
    
    * corrección de DataSchema
    
    * DATASCHEMA corrected
    
    * Error added when no type is present in the jsonschema
    
    * add test for different instance data.
    
    Co-authored-by: pchtsp <pchtsp@gmail.com>

---
+++ cornflow/schemas/constants.py
@@ -0,0 +1,14 @@
+"""
+Constants values used in schemas functions.
+"""
+
+STRING_TYPE = 'String'
+BOOLEAN_TYPE = 'Boolean'
+INTEGER_TYPE = 'Integer'
+FLOAT_TYPE = 'Float'
+BASIC_TYPES = [STRING_TYPE, BOOLEAN_TYPE, INTEGER_TYPE, FLOAT_TYPE]
+
+JSON_TYPES = {"string": STRING_TYPE, "number": FLOAT_TYPE, "integer": INTEGER_TYPE, "null": None,
+              "boolean": BOOLEAN_TYPE}
+
+DATASCHEMA = "DataSchema"
--- cornflow/tests/unit/test_schemas.py
+++ cornflow/tests/unit/test_schemas.py
@@ -1,7 +1,9 @@
 import unittest
 import json
 
-from cornflow.schemas.json_factory import gen_schema, ParameterSchema, sort_dict
+from cornflow.schemas.schema_dict_functions import gen_schema, ParameterSchema, sort_dict
+from cornflow.schemas.solution_log import LogSchema
+from airflow_config.dags.model_functions import solve_model
 from cornflow.schemas.solution_log import LogSchema
 from airflow_config.dags.model_functions import solve_model
 from marshmallow import ValidationError, Schema, fields
