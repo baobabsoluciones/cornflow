6a2f8d3b4dca215f8a47b7331f59df2afa743713
pchtsp <pchtsp@gmail.com>
2021-02-18 19:42:54 +0100

    some changes to be able to pass integration tests. Hopefully.

--- airflow_config/dags/update_all_schemas.py
+++ airflow_config/dags/update_all_schemas.py
@@ -15,7 +15,7 @@ default_args = {
     'email_on_retry': False,
     'retries': -1,
     'retry_delay': timedelta(minutes=1),
-    'schedule_interval': "@hourly"
+    'schedule_interval': "@once"
 }
 
 
