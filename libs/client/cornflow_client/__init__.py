8b82ec3e0679314255a9f56f55445803fd3799d6
pchtsp <pchtsp@gmail.com>
2021-03-15 19:29:04 +0100

    added exporting pulp_json_schema. Added MANIFEST.in to be sure to keep file.

--- libs/client/cornflow_client/__init__.py
+++ libs/client/cornflow_client/__init__.py
@@ -1,2 +1,11 @@
 from .cornflow_client import CornFlow, group_variables_by_name, CornFlowApiError
 from .schema_manager import SchemaManager
+import json
+import os
+
+
+def get_pulp_jsonschema():
+    filename = os.path.join(os.path.dirname(__file__), 'data', "pulp_json_schema.json")
+    with open(filename, 'r') as f:
+        content = json.load(f)
+    return content
