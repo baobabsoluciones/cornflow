9d971afe04e68ddcec4f144e4768623484fa0323
pchtsp <pchtsp@gmail.com>
2021-05-25 21:46:17 +0200

    fixed schema filtering by patching airflow call.

--- cornflow/tests/custom_test_case.py
+++ cornflow/tests/custom_test_case.py
@@ -7,6 +7,7 @@ from cornflow.shared.utils import db
 from cornflow.shared.authentication import Auth
 from cornflow.tests.const import LOGIN_URL
 from datetime import datetime, timedelta
+from unittest.mock import patch, Mock
 
 try:
     date_from_str = datetime.fromisoformat
@@ -226,10 +227,15 @@ class BaseTestCases:
             allrows = self.get_rows(self.url, data_many)
             self.apply_filter(self.url, dict(offset=1, limit=2), allrows.json[1:3])
 
-        def test_opt_filters_schema(self):
+        @patch("cornflow.endpoints.instance.get_schema")
+        def test_opt_filters_schema(self, get_schema):
+            # (we patch the request to airflow to check if the schema is valid)
             # we create 4 instances
+            mock = Mock()
+            mock.load.return_value = True
+            get_schema.return_value = lambda: mock
             data_many = [self.payload for _ in range(4)]
-            data_many[-1] = {**data_many[0], **dict(schema="timer")}
+            data_many[-1] = {**data_many[-1], **dict(schema="timer")}
             allrows = self.get_rows(self.url, data_many)
             self.apply_filter(self.url, dict(schema="timer"), allrows.json[:1])
 
@@ -263,5 +269,3 @@ class BaseTestCases:
                 allrows.json[:2],
             )
             return
-
-        pass
