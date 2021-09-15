191dd4a8f1e7c432846f5b1b22b65f0757ccf609
Ampuerin <ampuerin@gmail.com>
2021-09-15 16:05:36 +0200

    remove scip from solver installation list

--- airflow_config/scripts/solver_installation.py
+++ airflow_config/scripts/solver_installation.py
@@ -4,14 +4,13 @@ from solvers import get_cbc,get_glpk,get_highs,get_scip,get_choco,get_mipcl
 # list of available solvers
     # * HiGHS: https://www.maths.ed.ac.uk/hall/HiGHS/
     # * CBC: https://projects.coin-or.org/Cbc
-    # * SCIP: https://www.scipopt.org/
     # * CHOCO: https://github.com/chocoteam/choco-solver
     # * MIPCL: https://github.com/onebitbrain/MIPCL/blob/master/bin/mps_mipcl
     # * glpk: https://www.gnu.org/software/glpk/    
-available_solver = ['HiGHS','CBC','SCIP','CHOCO','MIPCL','glpk']
+available_solver = ['HiGHS','CBC','CHOCO','MIPCL','glpk']
 
 # list of solvers that will be installed
-solver_list = os.getenv('SOLVER_LIST','CBC,glpk,HiGHS,SCIP,CHOCO,MIPCL').split(',')
+solver_list = os.getenv('SOLVER_LIST','CBC,glpk,HiGHS,CHOCO,MIPCL').split(',')
 
 def install(s):
     if s in 'CBC':
@@ -20,8 +19,6 @@ def install(s):
         get_glpk.install()
     if s in 'HiGHS':
         get_highs.install()
-    if s in 'SCIP':
-        get_scip.install()
     if s in 'CHOCO':
         get_choco.install()
     if s in 'MIPCL':
