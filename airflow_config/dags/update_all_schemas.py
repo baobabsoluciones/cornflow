fc6f37a5d4a40662621d0562cee4541a954fdd37
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2021-12-14 13:54:25 +0100

    Deleted unused DAGS and updated requirements.txt

--- airflow_config/dags/update_all_schemas.py
+++ airflow_config/dags/update_all_schemas.py
@@ -1,50 +1,64 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator
-from airflow.models import Variable
-
-from datetime import datetime, timedelta
+# General imports
 import importlib as il
 import os
+import sys
+
+# Partial imports
+from airflow.operators.python import PythonOperator
+from airflow.models import Variable
+from airflow import DAG
+from airflow.utils.db import create_session
+from datetime import datetime, timedelta
+from typing import List
+
+# Import from cornflow environment
+from cornflow_client import ApplicationCore
 
 
 default_args = {
-    'owner': 'baobab',
-    'depends_on_past': False,
-    'start_date': datetime(2020, 2, 1),
-    'email': [''],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': -1,
-    'retry_delay': timedelta(minutes=1),
-    'schedule_interval': "@once",
+    "owner": "baobab",
+    "depends_on_past": False,
+    "start_date": datetime(2020, 2, 1),
+    "email": [""],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": -1,
+    "retry_delay": timedelta(minutes=1),
+    "schedule_interval": "@hourly",
+    "catchup": False,
 }
 
-schemas = ['instance', 'solution', 'config']
+schemas = ["instance", "solution", "config"]
 
 
-def get_all_apps():
+def get_new_apps() -> List[ApplicationCore]:
+    # we need to run this to be sure to import modules
+    import_dags()
+    new_apps = ApplicationCore.__subclasses__()
+    return [app_class() for app_class in new_apps]
+
+
+def import_dags():
+    sys.path.append(os.path.dirname(__file__))
     _dir = os.path.dirname(__file__)
     print("looking for apps in dir={}".format(_dir))
-    print("Files are: {}".format(os.listdir(_dir)))
     files = os.listdir(_dir)
-    return [_import_file(os.path.splitext(f)[0]) for f in files if is_app(f)]
+    print("Files are: {}".format(files))
+    # we go file by file and try to import it if matches the filters
+    for dag_module in files:
+        filename, ext = os.path.splitext(dag_module)
+        if ext not in [".py", ""]:
+            continue
+        if filename in ["activate_apps"]:
+            continue
+        try:
+            _import_file(filename)
+        except Exception as e:
+            continue
 
 
 def _import_file(filename):
     return il.import_module(filename)
-
-
-def is_app(dag_module):
-    filename, ext = os.path.splitext(dag_module)
-    if ext != '.py':
-        return False
-    try:
-        _module = _import_file(filename)
-        _module.name
-        _module.instance
-        return True
-    except Exception as e:
-        return False
 
 
 def get_schemas_dag_file(_module):
@@ -53,30 +67,41 @@ def get_schemas_dag_file(_module):
 
 
 def get_all_schemas():
-    apps = get_all_apps()
+    apps = get_new_apps()
     if len(apps):
         print("Found the following apps: {}".format([app.name for app in apps]))
     else:
         print("No apps were found to update")
-    schemas = [(dag_module.name, get_schemas_dag_file(dag_module)) for dag_module in apps]
-    return schemas
+    schemas_new = {app.name: app.get_schemas() for app in apps}
+    print("Found the following new apps: {}".format([app.name for app in apps]))
+    return schemas_new
 
 
 def update_schemas(**kwargs):
     schemas = get_all_schemas()
-    for key, value in schemas:
+    # we update all schemas that we found:
+    for key, value in schemas.items():
         Variable.set(key=key, value=value, serialize_json=True)
+    # now we clean the variables that do not exist anymore:
+    with create_session() as session:
+        current_vars = set(var.key for var in session.query(Variable))
+        apps_to_delete = current_vars - schemas.keys()
+        print("About to delete old apps: {}".format(apps_to_delete))
+        for _var in apps_to_delete:
+            Variable.delete(_var, session)
 
 
-dag = DAG('update_all_schemas', default_args=default_args, catchup=False)
+dag = DAG(
+    "update_all_schemas", default_args=default_args, catchup=False, tags=["internal"]
+)
 
 update_schema2 = PythonOperator(
-    task_id='update_all_schemas',
+    task_id="update_all_schemas",
     provide_context=True,
     python_callable=update_schemas,
     dag=dag,
 )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     update_schemas()
