efe37dbda5d71bcc1dfd36833db3424f047d1153
Guillermo González-Santander de la Cruz <guillermo.gonzalez@baobabsoluciones.es>
2022-01-19 08:56:40 +0100

    Feature/deployed dag endpoints (#188)
    * Reactivated integration tests
    
    * Changed name
    
    * Renamed DAG file
    
    * Changed version of requirements.txt on airflow to point to new version of cornflow-client
    
    * More debugging of integration tests
    
    * Test on github actions
    
    * Modified port on testing
    
    * Small changes to debug
    
    * Undo change
    
    * Added new service user
    
    * Small change
    
    * Updated password to contain all the requirements
    
    * Reactivated test
    
    * Fixed encoding test
    
    * Finishing touches on PR
    
    * Added get to deployed dag model
    
    * Fixed typo
    
    * More info on endpoint
    
    * Added post endpoint to deployed dag
    
    * Added permissions to service role to post on deployed dag endpoint
    
    * Updated schema and deployment script
    
    * Undo change
    
    * Added unit tests for the deployed dags endpoints

--- cornflow/schemas/__init__.py
+++ cornflow/schemas/__init__.py
@@ -2,8 +2,10 @@
 Initialization file for the schemas module
 """
 
+from .dag import DeployedDAGSchema
 from .execution import ExecutionSchema
 from .instance import InstanceSchema
 from .user import UserSchema
+
 
 # from .model_json import DataSchema
