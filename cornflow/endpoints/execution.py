9c86f9d77973dbd38b997a633d5821ab6e7f79cd
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2022-02-08 11:12:00 +0100

    Added new AIRFLOW status mapping

--- cornflow/endpoints/execution.py
+++ cornflow/endpoints/execution.py
@@ -306,7 +306,7 @@ class ExecutionStatusEndpoint(MetaResource, MethodResource):
 
         data = response.json()
         state = AIRFLOW_TO_STATE_MAP.get(data["state"], EXEC_STATE_UNKNOWN)
-        execution.update_state(state, message=data["state"])
+        execution.update_state(state)
         return execution, 200
 
 
