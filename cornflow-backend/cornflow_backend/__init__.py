70223805ae778294c6fe185b6bff2117ffdef6d1
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2022-04-05 18:29:30 +0200

    Moved Exceptions

--- cornflow-backend/cornflow_backend/__init__.py
+++
@@ -1,2 +0,0 @@
-from .models import EmptyBaseModel, TraceAttributesModel, UserBaseModel
-from .shared import database, password_crypt
--- cornflow-backend/cornflow_backend/shared/exceptions.py
+++ cornflow-backend/cornflow_backend/shared/exceptions.py
@@ -20,14 +20,65 @@ class InvalidUsage(Exception):
         return rv
 
 
+class AirflowError(InvalidUsage):
+    status_code = 400
+
+    def __init__(self, error, status_code=None, payload=None):
+        self.error = error
+        self.payload = payload
+        if status_code is not None:
+            self.status_code = status_code
+
+
+class ObjectDoesNotExist(InvalidUsage):
+    status_code = 404
+    error = "The object does not exist"
+
+
+class ObjectAlreadyExists(InvalidUsage):
+    status_code = 400
+    error = "The object does exist already"
+
+
+class NoPermission(InvalidUsage):
+    status_code = 403
+    error = "You have no permission to access the required object"
+
+
 class InvalidCredentials(InvalidUsage):
     status_code = 400
     error = "Invalid credentials"
 
 
+class EndpointNotImplemented(InvalidUsage):
+    status_code = 501
+    error = "Endpoint not implemented"
+
+
+class InvalidData(InvalidUsage):
+    status_code = 400
+    error = "The data sent is not valid"
+
+
+class CommunicationError(InvalidUsage):
+    status_code = 400
+    error = "The communication failed"
+
+
+class InvalidPatch(InvalidUsage):
+    status_code = 400
+    error = "The json patch sent is not valid"
+
+
 def initialize_errorhandlers(app):
     @app.errorhandler(InvalidUsage)
+    @app.errorhandler(ObjectDoesNotExist)
+    @app.errorhandler(NoPermission)
     @app.errorhandler(InvalidCredentials)
+    @app.errorhandler(EndpointNotImplemented)
+    @app.errorhandler(AirflowError)
+    @app.errorhandler(InvalidData)
+    @app.errorhandler(InvalidPatch)
     def handle_invalid_usage(error):
         response = jsonify(error.to_dict())
         response.status_code = error.status_code
