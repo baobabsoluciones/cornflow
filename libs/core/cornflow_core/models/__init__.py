51e842efce169fb24733184bc3bdef7a7b953668
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2022-04-06 19:42:53 +0200

    Changed import order

--- libs/core/cornflow_core/models/__init__.py
+++ libs/core/cornflow_core/models/__init__.py
@@ -1,6 +1,9 @@
-from .action import ActionBaseModel
+# Import generic first
 from .meta_models import EmptyBaseModel
 from .meta_models import TraceAttributesModel
+
+# Import particular after
+from .action import ActionBaseModel
 from .role import RoleBaseModel
 from .user import UserBaseModel
 from .view import ViewBaseModel
