2a359f3585ef640ccd6106d8a6accb36492e6ee0
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2022-04-06 19:36:39 +0200

    Changed action and view models

--- cornflow-server/cornflow/models/action.py
+++ cornflow-server/cornflow/models/action.py
@@ -1,13 +1,13 @@
 """
 
 """
-from cornflow_core.models import EmptyBaseModel
+from cornflow_core.models import ActionBaseModel
 
 # from .meta_model import EmptyModel
 from ..shared.utils import db
 
 
-class ActionModel(EmptyBaseModel):
+class ActionModel(ActionBaseModel):
     """
     This model contains the base actions over the REST API. These are:
 
@@ -20,9 +20,6 @@ class ActionModel(EmptyBaseModel):
 
     __tablename__ = "actions"
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(128), unique=True, nullable=False)
-
     permissions = db.relationship(
         "PermissionViewRoleModel",
         backref="actions",
@@ -31,14 +28,3 @@ class ActionModel(EmptyBaseModel):
         "PermissionViewRoleModel.deleted_at==None)",
         cascade="all,delete",
     )
-
-    def __repr__(self):
-        return str(self.id) + ": " + self.name
-
-    @classmethod
-    def get_all_objects(cls):
-        return cls.query.all()
-
-    @classmethod
-    def get_one_object(cls, idx):
-        return cls.query.get(idx)
--- cornflow-server/cornflow/models/apiview.py
+++ cornflow-server/cornflow/models/apiview.py
@@ -4,23 +4,19 @@
 # Import from libraries
 from sqlalchemy.dialects.postgresql import TEXT
 
-from cornflow_core.models import EmptyBaseModel
+from cornflow_core.models import ViewBaseModel
 
 # Import from internal modules
 # from .meta_model import EmptyModel
 from ..shared.utils import db
 
 
-class ApiViewModel(EmptyBaseModel):
+class ApiViewModel(ViewBaseModel):
     """
     This model should contain all the views by name declared in the endpoints init
     """
 
     __tablename__ = "api_view"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(128), unique=True, nullable=False)
-    url_rule = db.Column(db.String(128), nullable=False)
-    description = db.Column(TEXT, nullable=True)
 
     permissions = db.relationship(
         "PermissionViewRoleModel",
@@ -30,26 +26,3 @@ class ApiViewModel(EmptyBaseModel):
         "PermissionViewRoleModel.deleted_at==None)",
         cascade="all,delete",
     )
-
-    def __init__(self, data):
-        super().__init__()
-        self.name = data.get("name")
-        self.url_rule = data.get("url_rule")
-        self.description = data.get("description")
-
-    def __eq__(self, other):
-        return (isinstance(other, self.__class__)) and (self.name == other.name)
-
-    def __neq__(self, other):
-        return self.name != other.name
-
-    def __repr__(self):
-        return self.name
-
-    @classmethod
-    def get_one_by_name(cls, name):
-        return cls.query.filter_by(name=name).first()
-
-    @classmethod
-    def get_all_objects(cls):
-        return cls.query.all()
