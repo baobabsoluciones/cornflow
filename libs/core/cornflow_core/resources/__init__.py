13442a9fc0d29bcd3e46fc1ddb2c5f5350bf3cb2
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2022-04-20 17:29:44 +0200

    Fixed error on recover password

--- libs/core/cornflow_core/resources/__init__.py
+++ libs/core/cornflow_core/resources/__init__.py
@@ -1,3 +1,10 @@
-from .log_in_base import LoginBaseEndpoint
+"""
+Expose the resources
+"""
+# First the generic ones
 from .meta_resource import BaseMetaResource
+
+# Then the specific
+from .log_in_base import LoginBaseEndpoint
+from .recover_password import RecoverPasswordBaseEndpoint
 from .sign_up_base import SignupBaseEndpoint
