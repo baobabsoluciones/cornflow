351b60d536431444944482958e40c35147a1688a
Alejandro Ferreira <alejandro.ferreira@baobabsoluciones.es>
2022-05-19 09:45:54 +0200

    Amended the comments on previous commits. Still missing unitary test.

--- cornflow-server/cornflow/app.py
+++ cornflow-server/cornflow/app.py
@@ -28,7 +28,7 @@ from .commands.users import (
     create_service_user_command,
 )
 from .commands.views import register_views_command
-from .config import app_config, DefaultConfig
+from .config import app_config
 from .endpoints import resources
 from .endpoints.login import LoginEndpoint, LoginOpenAuthEndpoint
 from .endpoints.signup import SignUpEndpoint
@@ -80,7 +80,8 @@ def create_app(env_name="development", dataconn=None):
     auth_type = app.config["AUTH_TYPE"]
 
     if auth_type == AUTH_DB:
-        if DefaultConfig.SIGNUP_ACTIVATED:
+        signup_activated = int(app.config["SIGNUP_ACTIVATED"])
+        if signup_activated == 1:
             api.add_resource(SignUpEndpoint, "/signup/", endpoint="signup")
         api.add_resource(LoginEndpoint, "/login/", endpoint="login")
     elif auth_type == AUTH_LDAP:
