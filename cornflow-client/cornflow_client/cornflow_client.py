7da8f759e8601102c016f4bdf73bc8bf216034b8
Alejandro Ferreira <alejandro.ferreira@baobabsoluciones.es>
2022-05-20 14:01:24 +0200

    Added update_status function on cornflow_client.py

--- cornflow-client/cornflow_client/cornflow_client.py
+++ cornflow-client/cornflow_client/cornflow_client.py
@@ -190,8 +190,7 @@ class CornFlow(object):
             return response.json()
         raise CornFlowApiError(
             f"Connection failed with status code: {response.status_code}: {response.text}"
-            )
-
+        )
 
     @prepare_encoding
     def login(self, username, pwd, encoding=None):
@@ -474,6 +473,18 @@ class CornFlow(object):
             api="execution/", id=execution_id, post_url="status", encoding=encoding
         )
         return response.json()
+
+    def update_status(self, execution_id, payload, encoding=None):
+        """
+        Updates the status of the execution from queued to running when solved
+
+        :param str execution_id: id for the execution
+        :param int payload: code of the updated status for the execution
+        """
+        response = self.put_api_for_id(
+            api="execution/", id=execution_id, payload=payload, encoding=encoding
+        )
+        return response.json
 
     @log_call
     @ask_token
