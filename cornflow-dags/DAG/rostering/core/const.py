5eb92e70a277971a5959151f9d717865c26c6c0e
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2022-10-25 14:30:36 +0200

    Fixed to PK-FK

--- cornflow-dags/DAG/rostering/core/const.py
+++ cornflow-dags/DAG/rostering/core/const.py
@@ -1,7 +1,7 @@
 INSTANCE_KEYS_RELATION = {
     ("employees", "id"): [
         ("contracts", "employee_id"),
-        ("skills_employee", "id_employee"),
+        ("skills_employees", "id_employee"),
     ],
     ("shifts", "id"): [("contracts", "id_shift")],
     ("skills", "id"): [
