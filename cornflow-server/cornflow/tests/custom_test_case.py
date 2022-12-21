2201b21ee6bc04e1b8ebc54efa2e14ee0bd13489
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2022-12-21 15:18:35 +0100

    Added test for the failed login of service user against the base user ldap configuration

--- cornflow-server/cornflow/tests/custom_test_case.py
+++ cornflow-server/cornflow/tests/custom_test_case.py
@@ -571,6 +571,17 @@ class LoginTestCases:
             self.assertEqual(200, self.response.status_code)
             self.assertEqual(str, type(self.response.json["token"]))
 
+        def test_failed_log_in(self):
+            payload = self.data
+            self.response = self.client.post(
+                LOGIN_URL,
+                data=json.dumps(payload),
+                follow_redirects=True,
+                headers={"Content-Type": "application/json"},
+            )
+
+            self.assertEqual(400, self.response.status_code)
+
         def test_validation_error(self):
             payload = self.data
             payload["email"] = "test"
