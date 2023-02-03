064ccff1fff362b455b65592c847087587e92476
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2023-02-03 11:24:23 +0100

    Changes to commands to make it a module

---
+++ cornflow-server/cornflow/commands/__init__.py
@@ -0,0 +1,16 @@
+from .access import access_init_command
+from .actions import register_actions_command
+from .dag import register_deployed_dags_command
+from .permissions import (
+    register_base_permissions_command,
+    register_dag_permissions_command,
+)
+from .roles import register_roles_command
+from .schemas import update_schemas_command
+from .users import (
+    create_user_with_role,
+    create_service_user_command,
+    create_admin_user_command,
+    create_planner_user_command,
+)
+from .views import register_views_command
