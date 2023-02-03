d7fbe4ee0f7921ad8dc1e96d9c03b1e0dd78c85b
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2023-02-03 10:31:44 +0100

    Test the isntallation of cornflow as a package

---
+++ cornflow-server/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.rst") as fh:
+    long_description = fh.read()
+
+required = []
+with open("requirements.txt", "r") as fh:
+    required.append(fh.read().splitlines())
+
+setuptools.setup(
+    name="cornflow",
+    version="0.0.1a0",
+    author="baobab soluciones",
+    author_email="sistemas@baobabsoluciones.es",
+    url="http://github.com/baobabsoluciones/cornflow",
+    packages=setuptools.find_packages(),
+    install_requires=required,
+    include_package_data=True,
+)
