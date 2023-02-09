12908fe6605c19a82fe551e96faf49a5c305b94e
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2023-02-09 23:55:11 +0100

    Applied black format to old migration

--- cornflow-server/cornflow/migrations/versions/7c3ea5ab5501_.py
+++ cornflow-server/cornflow/migrations/versions/7c3ea5ab5501_.py
@@ -11,32 +11,46 @@ from sqlalchemy.dialects import postgresql
 
 
 # revision identifiers, used by Alembic.
-revision = '7c3ea5ab5501'
-down_revision = 'd0e0700dcd8e'
+revision = "7c3ea5ab5501"
+down_revision = "d0e0700dcd8e"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
     op.add_column(
-        'deployed_dags',
-        sa.Column('instance_schema', postgresql.JSON(astext_type=sa.Text()), nullable=True)
+        "deployed_dags",
+        sa.Column(
+            "instance_schema", postgresql.JSON(astext_type=sa.Text()), nullable=True
+        ),
     )
     op.add_column(
-        'deployed_dags',
-        sa.Column('solution_schema', postgresql.JSON(astext_type=sa.Text()), nullable=True)
+        "deployed_dags",
+        sa.Column(
+            "solution_schema", postgresql.JSON(astext_type=sa.Text()), nullable=True
+        ),
     )
     op.add_column(
-        'deployed_dags',
-        sa.Column('config_schema', postgresql.JSON(astext_type=sa.Text()), nullable=True)
+        "deployed_dags",
+        sa.Column(
+            "config_schema", postgresql.JSON(astext_type=sa.Text()), nullable=True
+        ),
     )
     op.add_column(
-        'deployed_dags',
-        sa.Column('instance_checks_schema', postgresql.JSON(astext_type=sa.Text()), nullable=True)
+        "deployed_dags",
+        sa.Column(
+            "instance_checks_schema",
+            postgresql.JSON(astext_type=sa.Text()),
+            nullable=True,
+        ),
     )
     op.add_column(
-        'deployed_dags',
-        sa.Column('solution_checks_schema', postgresql.JSON(astext_type=sa.Text()), nullable=True)
+        "deployed_dags",
+        sa.Column(
+            "solution_checks_schema",
+            postgresql.JSON(astext_type=sa.Text()),
+            nullable=True,
+        ),
     )
 
 
