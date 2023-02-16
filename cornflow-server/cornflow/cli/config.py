2708391e2bb97ad1f6cc3301e63479e7287e5af0
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2023-02-16 16:50:53 +0100

    Added more commands for the configuration

--- cornflow-server/cornflow/cli/config.py
+++ cornflow-server/cornflow/cli/config.py
@@ -7,7 +7,7 @@ from flask import current_app
 
 @click.command(name="config_list", help="List the configuration variables")
 def config_list():
-    env = os.getenv("FLASK_ENV", None)
+    env = os.getenv("FLASK_ENV", "development")
     if env is None:
         click.echo("The environment variables are not set")
         return 0
@@ -18,3 +18,41 @@ def config_list():
             click.echo(f"{key} = {value}")
 
     return 1
+
+
+@click.command(name="config_save", help="Save the configuration variables to a file")
+@click.option(
+    "--path",
+    "-p",
+    type=str,
+    help="The path to the file to save the config",
+    default="./",
+)
+def config_save(path):
+    env = os.getenv("FLASK_ENV", "development")
+    if env is None:
+        click.echo("The environment variables are not set")
+        return 0
+
+    app = create_app(env)
+    path = f"{path}config.cfg"
+    with app.app_context():
+        with open(path, "w") as f:
+            f.write("[configuration]\n")
+            for key, value in current_app.config.items():
+                f.write(f"{key} = {value}")
+
+    return 1
+
+
+@click.command(name="config_get", help="Get the value of a configuration variable")
+@click.option("--key", "-k", type=str, help="The key of the configuration variable")
+def config_get(key):
+    env = os.getenv("FLASK_ENV", "development")
+    if env is None:
+        click.echo("The environment variables are not set")
+        return 0
+
+    app = create_app(env)
+    with app.app_context():
+        click.echo(f"{current_app.config.get(key, None)}")
