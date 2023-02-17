2572f25877498d45644e265cb387b2a7e583b0f4
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2023-02-17 12:54:23 +0100

    Added roles group to cli

--- cornflow-server/cornflow/cli/__init__.py
+++ cornflow-server/cornflow/cli/__init__.py
@@ -5,6 +5,7 @@ init file to have this as a module
 import click
 from cornflow.cli.migrations import migrations
 from cornflow.cli.config import config
+from cornflow.cli.roles import roles
 from cornflow.cli.service import service
 from cornflow.cli.permissions import permissions
 from cornflow.cli.users import users
@@ -18,5 +19,6 @@ def cli():
 cli.add_command(config)
 cli.add_command(migrations)
 cli.add_command(permissions)
+cli.add_command(roles)
 cli.add_command(service)
 cli.add_command(users)
