5f5f26a399006a60d5db8f2474875448175afcb3
Jaime Sotomayor <jaime.sotomayor@baobabsoluciones.es>
2023-03-01 11:57:17 +0100

    DAG that executes scripts automatically

---
+++ cornflow-dags/DAG/auto_scripts/automatic_scripts.py
@@ -0,0 +1,164 @@
+import os
+import shutil
+
+from sqlalchemy import create_engine
+from sqlalchemy.exc import SQLAlchemyError
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+# Default arguments of DAG
+default_args = {
+    "owner": "baobab",
+    "depends_on_past": False,
+    "start_date": datetime(2023, 1, 1),
+    "email": [""],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": -1,
+    "retry_delay": timedelta(minutes=1),
+    "catchup": False,
+}
+# Folder path
+SCRIPT_FOLDER = "/usr/local/airflow/dags/automatic_scripts/scripts_to_execute"
+DESTINATION_FOLDER = "/usr/local/airflow/dags/automatic_scripts/executed_scripts"
+CONN = os.environ.get(
+    "CORNFLOW_DB_URI", "postgresql://{uid}:{pwd}@{hostname}:{port}/{database}"
+)
+
+
+def move_script(auto_script, log="", error=0):
+    # Get current date and time
+    now = datetime.now()
+
+    # Format date and time as string
+    datetime_str = now.strftime("%Y%m%d_%H%M")
+
+    # Name of folder to create. If the script had an error, the name of the folder flags it
+    if error:
+        folder_name = f"ERROR_{os.path.splitext(auto_script)[0]}_{datetime_str}"
+    else:
+        folder_name = f"{os.path.splitext(auto_script)[0]}_{datetime_str}"
+
+    # Full path of folder to create
+    folder_path = os.path.join(DESTINATION_FOLDER, folder_name)
+
+    # Create folder if it doesn't exist
+    if not os.path.exists(folder_path):
+        os.mkdir(folder_path)
+        print(f"Folder created successfully at {folder_path}")
+    else:
+        print(f"Folder '{folder_path}' already exists.")
+
+    # Move script to folder
+    script_path = os.path.join(SCRIPT_FOLDER, auto_script)
+    shutil.move(script_path, folder_path)
+    # Export log
+    log_path = os.path.join(folder_path, "log.txt")
+    with open(log_path, "w") as file:
+        # Write log
+        file.write(log)
+    print(f"Script {auto_script} moved out of folder")
+
+
+def execute_scripts():
+    # Iterate directory
+    for auto_script in os.listdir(SCRIPT_FOLDER):
+        error = 0
+        print(f"Executing {auto_script}")
+        # Check if current path is a file
+        if os.path.isfile(os.path.join(SCRIPT_FOLDER, auto_script)):
+            script_path = os.path.join(SCRIPT_FOLDER, auto_script)
+
+            # Check if python file
+            if ".py" in auto_script:
+                try:
+                    # Execute script
+                    stream = os.popen(f"python {script_path}")
+                    log = stream.read()
+                    # Print the results
+                    print(log)
+
+                except Exception as e:
+                    print(f"Something went wrong: {str(e)}")
+                    log = str(e)
+                    error = 1
+
+            elif ".sql" in auto_script:
+                log = ""
+                # Read the SQL file
+                with open(script_path, "r") as f:
+                    query = f.read()
+
+                try:
+                    # Connect to the database
+                    engine = create_engine(CONN)
+
+                except SQLAlchemyError as e:
+
+                    # Print the error message
+                    print(f"Error: {e}")
+                    log += str(e)
+                    error = 1
+                    move_script(auto_script, log, error)
+                    continue
+
+                # All SQL commands (split on ';')
+                sqlCommands = query.split(";")
+                for command in sqlCommands:
+                    # Check empty query
+                    if command == "":
+                        continue
+                    # Begin a transaction
+                    with engine.connect() as connection:
+                        transaction = connection.begin()
+                        try:
+                            # Execute the SQL query
+                            print(f"Query: {command}")
+                            connection.execute(command)
+                            # Commit the transaction
+                            transaction.commit()
+                            message = "Query executed successfully!"
+                            log += message
+                            print(message)
+
+                        except SQLAlchemyError as e:
+                            # Rollback the transaction if the query is not successful
+                            transaction.rollback()
+
+                            # Print the error message
+                            print(f"Error: {e}")
+                            log += str(e)
+                            error = 1
+
+            else:
+                print(
+                    f"The script {auto_script} is not recognized as either a python file or a sql file"
+                )
+                # We do nothing with the file
+                continue
+            # Move script and create log
+            move_script(auto_script, log, error)
+            print(f"End of {auto_script} execution.")
+        else:
+            print(f"{auto_script} is not a file")
+
+
+dag = DAG(
+    "execute_scripts",
+    default_args=default_args,
+    catchup=False,
+    tags=["internal"],
+    schedule_interval="@hourly",
+)
+
+update_schema2 = PythonOperator(
+    task_id="execute_scripts",
+    provide_context=True,
+    python_callable=execute_scripts,
+    dag=dag,
+)
+
+
+if __name__ == "__main__":
+    execute_scripts()
