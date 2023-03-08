9e941396af3bd4bb73118b74749b18ddf934cee1
JaimeSotomayor <57803565+JaimeSotomayor@users.noreply.github.com>
2023-03-08 16:04:48 +0100

    Feature/s7 rostering schedule2.0 js (#401)
    * DAG that executes scripts automatically
    
    * Bug without db_uri
    
    * Change of popen to system.
    Correction for capturing error
    
    * error correction
    Included test
    
    * Remove prints
    
    * Changes for having weekly schedule.
    Added solution for instance 3
    
    * Warning if slot lenght is not multiple of 5
    
    * Remove old parameters from schema
    
    * Included to_dict() tables
    Added solutions to tests
    
    * changed to isoweek
    
    * Changes to test instances
    
    * Last fix to test instance_2
    
    * Error when exception missing
    
    * request for ending date
    
    ---------
    
    Co-authored-by: Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>

--- cornflow-dags/DAG/rostering/__init__.py
+++ cornflow-dags/DAG/rostering/__init__.py
@@ -3,7 +3,7 @@
 """
 # Imports from libraries
 import os
-from typing import Dict, List
+from typing import Dict, List, Tuple, Union
 
 # Imports from cornflow libraries
 from cornflow_client import ApplicationCore
@@ -23,25 +23,26 @@ class Rostering(ApplicationCore):
     schema = load_json(os.path.join(os.path.dirname(__file__), "./schemas/config.json"))
 
     @property
-    def test_cases(self) -> List[Dict]:
+    def test_cases(self) -> List[Union[Dict, Tuple[Dict, Dict]]]:
         data1 = load_json(
             os.path.join(os.path.dirname(__file__), "data/test_instance_1.json")
         )
         data_out1 = load_json(
             os.path.join(os.path.dirname(__file__), "data/test_solution_1.json")
         )
-        data2 = load_json(
+
+        data_2 = load_json(
             os.path.join(os.path.dirname(__file__), "data/test_instance_2.json")
         )
-        data_3 = load_json(
+
+        data3 = load_json(
             os.path.join(os.path.dirname(__file__), "data/test_instance_3.json")
         )
-
-        data_5 = load_json(
-            os.path.join(os.path.dirname(__file__), "data/test_instance_5.json")
+        data_out3 = load_json(
+            os.path.join(os.path.dirname(__file__), "data/test_solution_3.json")
         )
 
-        return [(data1, data_out1), data2, data_3, data_5]
+        return [(data1, data_out1), data_2, (data3, data_out3)]
 
     def get_solver(self, name: str = "mip"):
         if "." in name:
