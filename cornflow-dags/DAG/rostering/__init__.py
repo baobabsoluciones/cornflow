90445d597c2cf5c559d652fb1c2c00615c64a7f0
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2023-03-15 12:02:23 +0100

    Fixed test instances

--- cornflow-dags/DAG/rostering/__init__.py
+++ cornflow-dags/DAG/rostering/__init__.py
@@ -46,7 +46,11 @@ class Rostering(ApplicationCore):
             os.path.join(os.path.dirname(__file__), "data/test_instance_7.json")
         )
 
-        return [(data1, data_out1), data_2, (data3, data_out3), data_7]
+        data_8 = load_json(
+            os.path.join(os.path.dirname(__file__), "data/test_instance_8.json")
+        )
+
+        return [(data1, data_out1), data_2, (data3, data_out3), data_7, data_8]
 
     def get_solver(self, name: str = "mip"):
         if "." in name:
