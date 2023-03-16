35b97c4030f859a278ecbd005d934b1133c656c4
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2023-03-16 22:41:18 +0100

    Debug health endpoint

--- cornflow-server/cornflow/endpoints/health.py
+++ cornflow-server/cornflow/endpoints/health.py
@@ -12,19 +12,31 @@ from cornflow.schemas.health import HealthResponse
 from cornflow.shared.const import STATUS_HEALTHY, STATUS_UNHEALTHY
 from cornflow_core.shared import db
 from cornflow_core.resources import BaseMetaResource
+from cornflow.models import UserModel
 
 
 class HealthEndpoint(BaseMetaResource):
     @doc(description="Health check", tags=["Health"])
     @marshal_with(HealthResponse)
     def get(self):
+        current_app.logger.error("HEALTH ENDPOINT")
+        current_app.logger.error(f"{current_app.config['AIRFLOW_USER']}")
+        current_app.logger.error(f"{current_app.config['AIRFLOW_URL']}")
+        current_app.logger.error(f"{current_app.config['AIRFLOW_PWD']}")
+        print("HEALTH ENDPOINT")
+        print(f"{current_app.config['AIRFLOW_USER']}")
+        print(f"{current_app.config['AIRFLOW_URL']}")
+        print(f"{current_app.config['AIRFLOW_PWD']}")
         af_client = Airflow.from_config(current_app.config)
         airflow_status = STATUS_HEALTHY
         cornflow_status = STATUS_HEALTHY
         if not af_client.is_alive():
             airflow_status = STATUS_UNHEALTHY
         try:
-            db.engine.execute("SELECT 1")
+            if UserModel.get_one_user_by_username("service_user") is not None:
+                cornflow_status = STATUS_HEALTHY
+            else:
+                cornflow_status = STATUS_UNHEALTHY
         except Exception:
             cornflow_status = STATUS_UNHEALTHY
         return {"cornflow_status": cornflow_status, "airflow_status": airflow_status}
--- cornflow-server/setup.py
+++ cornflow-server/setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
     name="cornflow",
-    version="1.0.2a4",
+    version="1.0.2a5",
     author="baobab soluciones",
     author_email="cornflow@baobabsoluciones.es",
     description="Cornflow is an open source multi-solver optimization server with a REST API built using flask.",
