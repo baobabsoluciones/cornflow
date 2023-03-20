621d5ec49ab99b4a5453f7fd2c8b601f3ade9198
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2023-03-20 23:08:02 +0100

    To create version from local

--- cornflow-server/cornflow/cli/service.py
+++ cornflow-server/cornflow/cli/service.py
@@ -157,7 +157,7 @@ def init_cornflow_service():
 
         # execute gunicorn application
         os.system(
-            "/usr/local/bin/gunicorn -c python:cornflow.gunicorn \"cornflow.production:create_app('$FLASK_ENV')\""
+            "/usr/local/bin/gunicorn -c python:cornflow.gunicorn \"cornflow.local:create_app('$FLASK_ENV')\""
         )
 
     elif external_application == 1:
