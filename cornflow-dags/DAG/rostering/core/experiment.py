3678938fb9b95e46ff61040ddc8e8e5278c86dc6
marioncottard <86973971+marioncottard@users.noreply.github.com>
2023-10-04 17:52:25 +0200

    Feature/rostering soft constraints (#464)
    * Changed all hours from int to string
    
    * Soft constraints
    
    * Deleted methods that are in InstanceSolutionCore
    
    * Added output checks, Applied formatter
    
    * Cleaning
    
    * Updated readme
    
    * Cleaning
    
    * Docstrings
    
    * Adjustements for 24/7 store
    
    * Cleaning
    
    * Updated solution files
    
    * Fixed clients tests
    
    * Post merge changes
    
    * Undo changes
    
    * Small changes rostering
    
    ---------
    
    Co-authored-by: Guillermo Gonzalez-Santander <guillermo.gonzalez@baobabsoluciones.es>

--- cornflow-dags/DAG/rostering/core/experiment.py
+++ cornflow-dags/DAG/rostering/core/experiment.py
@@ -43,6 +43,12 @@ class Experiment(ExperimentCore):
             difference_hours_worked=self.check_hours_worked(),
             manager_present=self.check_manager_present(),
             skills_demand=self.check_skills_demand(),
+            days_worked_per_week=self.check_days_worked_per_week(),
+            min_hours_worked_per_day=self.check_min_hours_worked_day(),
+            employee_holidays=self.check_employee_holidays(),
+            employee_downtime=self.check_employees_downtime(),
+            start_hour_preference=self.check_start_hour_preference(),
+            number_hours_preferences=self.check_number_hours_preference(),
         ).vfilter(lambda v: len(v))
 
     def get_objective(self) -> float:
@@ -140,6 +146,120 @@ class Experiment(ExperimentCore):
             )
         else:
             return TupList([])
+
+    def check_days_worked_per_week(self):
+        """Checks if an employee works more days than the number of days on their contract"""
+        max_working_slots_week = self.instance.get_max_working_days()
+
+        return (
+            self.solution.get_ts_employee()
+            .to_tuplist()
+            .vapply_col(
+                None,
+                lambda v: self.instance.get_week_from_ts(
+                    self.instance.get_datetime_from_string(v[0])
+                ),
+            )
+            .to_dict(result_col=0, indices=[2, 1])
+            .vapply(lambda v: len(v.vapply(lambda vv: vv[:10]).unique()))
+            .kvfilter(lambda k, v: v > max_working_slots_week[k])
+            .to_tuplist()
+            .vapply(
+                lambda v: {
+                    "week": v[0],
+                    "id_employee": v[1],
+                    "nb_days_worked": v[2],
+                    "max_days_week": max_working_slots_week[v[0], v[1]],
+                }
+            )
+        )
+
+    def check_min_hours_worked_day(self):
+        """Checks if an employee works less than the minimum hours"""
+        min_worked_ts_day = self.instance.get_min_working_slots_day()
+
+        return (
+            self.solution.get_ts_employee()
+            .to_tuplist()
+            .vapply_col(None, lambda v: v[0][:10])
+            .to_dict(result_col=[0], indices=[2, 1])
+            .vapply(lambda v: len(v))
+            .kvfilter(lambda k, v: v < min_worked_ts_day[k])
+            .to_tuplist()
+            .vapply(
+                lambda v: {
+                    "date": v[0],
+                    "id_employee": v[1],
+                    "nb_hours_worked": self.instance.slot_to_hour(v[2]),
+                    "min_hours_day": self.instance.slot_to_hour(
+                        min_worked_ts_day[v[0], v[1]]
+                    ),
+                }
+            )
+        )
+
+    def check_employee_holidays(self):
+        """Checks that no employee works on their holidays"""
+        ts_employee_holidays = self.instance.get_employee_time_slots_holidays()
+        return (
+            self.solution.get_ts_employee()
+            .to_tuplist()
+            .vfilter(lambda v: v in ts_employee_holidays)
+            .vapply_col(None, lambda v: v[0][:10])
+            .vapply(lambda v: {"date": v[2], "id_employee": v[1]})
+        )
+
+    def check_employees_downtime(self):
+        """Checks that no employee works on their downtime"""
+        employee_downtime = self.instance._get_employee_downtime()
+
+        return (
+            self.solution.get_ts_employee()
+            .to_tuplist()
+            .vapply_col(None, lambda v: v[0][:10])
+            .unique()
+            .take([1, 2])
+            .vfilter(lambda v: v in employee_downtime)
+            .vapply(lambda v: {"date": v[1], "id_employee": v[0]})
+        )
+
+    def check_start_hour_preference(self):
+        """Checks that start hour preferences are respected"""
+        start_preferences = self.instance.get_employee_preference_start_ts()
+
+        return (
+            self.solution.get_ts_employee()
+            .to_tuplist()
+            .vapply_col(None, lambda v: v[0][:10])
+            .to_dict(result_col=0, indices=[2, 1])
+            .vapply(lambda v: min(v))
+            .kvfilter(lambda k, v: v not in start_preferences.get(k, [v]))
+            .to_tuplist()
+            .vapply(
+                lambda v: {"date": v[0], "id_employee": v[1], "start_hour": v[2][11:]}
+            )
+        )
+
+    def check_number_hours_preference(self):
+        """Checks that the preferred number of hours worked is respected"""
+        preference_hours_employee = self.instance.get_employee_preference_hours()
+
+        return (
+            self.solution.get_ts_employee()
+            .to_tuplist()
+            .vapply_col(None, lambda v: v[0][:10])
+            .to_dict(result_col=0, indices=[2, 1])
+            .vapply(lambda v: self.instance.slot_to_hour(len(v)))
+            .kvfilter(lambda k, v: v > preference_hours_employee.get(k, v))
+            .to_tuplist()
+            .vapply(
+                lambda v: {
+                    "date": v[0],
+                    "id_employee": v[1],
+                    "number_hours_worked": v[2],
+                }
+            )
+        )
 
     def get_one_employee_percentage(self) -> float:
         """Returns the percentage of time slots where only one employee is working"""
