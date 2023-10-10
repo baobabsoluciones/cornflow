135063bd4fa350aba41287cad3f642603db88ae4
Ampuerin <ampuerin@gmail.com>
2023-10-10 11:28:46 +0200

    config custom Airflow UI for CornflowEnv

---
+++ cornflow-server/airflow_config/airflow_local_settings.py
@@ -0,0 +1,17 @@
+# AIRFLOW custom UI colors for CornflowEnv
+
+STATE_COLORS = {
+    "deferred": "#7c4fe0",
+    "failed": "#c9301c",
+    "queued": "#656d78",
+    "removed": "#7bdcb5",
+    "restarting": "#9b51e0",
+    "running": "#01FF70",
+    "scheduled": "#fcb900",
+    "shutdown": "blue",
+    "skipped": "darkorchid",
+    "success": "#00d084",
+    "up_for_reschedule": "#f78da7",
+    "up_for_retry": "yellow",
+    "upstream_failed": "#b31e1e",
+}
