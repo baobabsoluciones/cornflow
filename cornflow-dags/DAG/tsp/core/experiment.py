ce04099b038a07016e71b58d7e6906917a92fa69
pchtsp <pchtsp@gmail.com>
2024-05-17 12:17:20 +0200

    first version of tsp report + changes (many more requirements added)

--- cornflow-dags/DAG/tsp/core/experiment.py
+++ cornflow-dags/DAG/tsp/core/experiment.py
@@ -25,31 +25,38 @@ class Experiment(ExperimentCore):
         self._solution = value
 
     def get_objective(self) -> float:
+        # if solution is empty, we return 0
+        if len(self.solution.data["route"]) == 0:
+            return 0
         # we get a sorted list of nodes by position
-        route = (
-            TupList(self.solution.data["route"])
-            .sorted(key=lambda v: v["pos"])
-            .vapply(lambda v: v["node"])
-        )
-        weight = {(el["n1"], el["n2"]): el["w"] for el in self.instance.data["arcs"]}
-        # we sum all arcs in the solution
-        return (
-            sum([weight[n1, n2] for n1, n2 in zip(route, route[1:])])
-            + weight[route[-1], route[0]]
-        )
+        arcs = self.solution.get_used_arcs()
+
+        # we sum all arc weights in the solution
+        return sum(self.get_used_arc_weights().values())
+
+    def get_used_arc_weights(self) -> dict:
+        arcs = self.solution.get_used_arcs()
+        weight = self.instance.get_indexed_arcs()
+        return arcs.to_dict(None).kapply(lambda k: weight[k]["w"])
 
     def check_missing_nodes(self):
         nodes_in = TupList(v["n1"] for v in self.instance.data["arcs"]).to_set()
         nodes_out = TupList(n["node"] for n in self.solution.data["route"]).to_set()
-        return [{"node": n} for n in (nodes_in - nodes_out)]
+        return TupList({"node": n} for n in (nodes_in - nodes_out))
 
     def check_missing_positions(self):
         nodes_in = TupList(v["n1"] for v in self.instance.data["arcs"]).to_set()
         positions = TupList(n["pos"] for n in self.solution.data["route"]).to_set()
-        return [{"position": p} for p in set(range(len(nodes_in))) - positions]
+        return TupList({"position": p} for p in set(range(len(nodes_in))) - positions)
 
     def check_solution(self, *args, **kwargs) -> SuperDict:
         return SuperDict(
             missing_nodes=self.check_missing_nodes(),
             missing_positions=self.check_missing_positions(),
         )
+
+    def get_report(self):
+        # get positions (explicit, or implicitly via distances)
+        # get graph of solution
+        #
+        pass
