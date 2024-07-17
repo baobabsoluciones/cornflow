74f2b5d0a3cab4e0ba9b14def033231ff42f3b49
Guillermo Gonzalez-Santander <guillermo.gonzalez@baobabsoluciones.es>
2024-07-17 09:29:41 +0200

    More debugging

--- cornflow-server/cornflow/endpoints/reports.py
+++ cornflow-server/cornflow/endpoints/reports.py
@@ -117,7 +117,7 @@ class ReportEndpoint(BaseMetaResource):
             report.save()
         except Exception as error:
             current_app.logger.error(error)
-            raise InvalidUsage("Error on POST report")
+            raise InvalidUsage(error=error)
 
         try:
             # We try to save the file, if an error is raised then we delete the record on the database
