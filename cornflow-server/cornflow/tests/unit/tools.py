1dabd3e9bcaf563c17a0037c33c4ac339f8fe688
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2024-10-28 16:34:42 +0000

    Starting run_workflow change

--- cornflow-server/cornflow/tests/unit/tools.py
+++ cornflow-server/cornflow/tests/unit/tools.py
@@ -7,7 +7,7 @@ def patch_af_client(af_client_class):
     responses_mock.json.return_value = {"is_paused": False, "dag_run_id": "12345", "state": "success"}
     af_client_mock.is_alive.return_value = True
     af_client_mock.get_orq_info.return_value = responses_mock
-    af_client_mock.run_dag.return_value = responses_mock
+    af_client_mock.run_workflow.return_value = responses_mock
     af_client_mock.get_dag_run_status.return_value = responses_mock
     af_client_mock.set_dag_run_to_fail.return_value = None
     af_client_class.from_config.return_value = af_client_mock
