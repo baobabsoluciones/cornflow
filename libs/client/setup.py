15b28beb91a5e6d3ea231446c42221e09d98aa1a
Guillermo González-Santander de la Cruz <guillermo.gonzalez@baobabsoluciones.es>
2024-12-05 17:39:55 +0100

    Bump version due to pypi outage (#584)

--- libs/client/setup.py
+++ libs/client/setup.py
@@ -12,7 +12,7 @@ extra_required = {"excel": ["openpyxl", "pandas"]}
 
 setuptools.setup(
     name="cornflow-client",
-    version="1.1.3",
+    version="1.1.4",
     author="baobab soluciones",
     author_email="sistemas@baobabsoluciones.es",
     description="Client to connect to a cornflow server",
