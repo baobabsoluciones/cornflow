37b61c837c12426071b771e66b31e9cf81e9fbd6
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2025-01-15 08:34:23 +0100

    Include requirements in MANIFEST

--- libs/client/setup.py
+++ libs/client/setup.py
@@ -12,7 +12,7 @@ extra_required = {"excel": ["openpyxl", "pandas"]}
 
 setuptools.setup(
     name="cornflow-client",
-    version="1.1.5a1",
+    version="1.1.5a2",
     author="baobab soluciones",
     author_email="sistemas@baobabsoluciones.es",
     description="Client to connect to a cornflow server",
