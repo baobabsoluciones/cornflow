08a83a583b2af8e36485013e0d8e9058f3facd7e
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2025-02-24 15:27:56 +0100

    Added a couple of extra tests

--- cornflow-server/cornflow/tests/custom_test_case.py
+++ cornflow-server/cornflow/tests/custom_test_case.py
@@ -596,6 +596,22 @@ class BaseTestCases:
             allrows = self.get_rows(self.url, data_many)
             self.apply_filter(self.url, dict(offset=1, limit=2), allrows.json[1:3])
 
+        def test_opt_filters_offset_zero(self):
+            """
+            Tests the offset filter option with a zero value.
+            """
+            data_many = [self.payload for _ in range(4)]
+            allrows = self.get_rows(self.url, data_many)
+            self.apply_filter(self.url, dict(offset=0), allrows.json)
+
+        def test_opt_filters_offset_none(self):
+            """
+            Tests the offset filter option with a None value.
+            """
+            data_many = [self.payload for _ in range(4)]
+            allrows = self.get_rows(self.url, data_many)
+            self.apply_filter(self.url, dict(offset=None), allrows.json)
+
         def test_opt_filters_schema(self):
             """
             Tests the schema filter option.
