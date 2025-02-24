a31744b8fd3e6ad60c0f512f96affc9058734b96
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2025-02-24 15:58:36 +0100

    Added some more tests

--- cornflow-server/cornflow/tests/unit/test_apiview.py
+++ cornflow-server/cornflow/tests/unit/test_apiview.py
@@ -14,6 +14,7 @@ TestApiViewListEndpoint
 
 # Import from internal modules
 from cornflow.endpoints import ApiViewListEndpoint, resources
+from cornflow.models import ViewModel
 from cornflow.shared.const import ROLES_MAP
 from cornflow.tests.const import APIVIEW_URL
 from cornflow.tests.custom_test_case import CustomTestCase
@@ -102,3 +103,75 @@ class TestApiViewListEndpoint(CustomTestCase):
                 )
 
                 self.assertEqual(403, response.status_code)
+
+
+class TestApiViewModel(CustomTestCase):
+    """
+    Test cases for the API views list endpoint.
+
+    This class tests the functionality of listing available API views, including:
+    - Authorization checks for different user roles
+    - Validation of returned API view data
+    - Access control for authorized and unauthorized roles
+    """
+
+    def setUp(self):
+        """
+        Set up test environment before each test.
+
+        Initializes test data including:
+        - Base test case setup
+        - Roles with access permissions
+        - Test payload with API view data
+        - Items to check in responses
+        """
+        super().setUp()
+        self.roles_with_access = ApiViewListEndpoint.ROLES_WITH_ACCESS
+        self.payload = [
+            {
+                "name": view["endpoint"],
+                "url_rule": view["urls"],
+                "description": view["resource"].DESCRIPTION,
+            }
+            for view in resources
+        ]
+        self.items_to_check = ["name", "description", "url_rule"]
+
+    def test_get_all_objects(self):
+        """
+        Test that the get_all_objects method works properly
+        """
+        objective = len(resources) + 2
+
+        instances = ViewModel.get_all_objects().all()
+        self.assertEqual(len(instances), objective)
+
+        instances = ViewModel.get_all_objects(offset=10, limit=10).all()
+        self.assertEqual(len(instances), 10)
+
+        instances = ViewModel.get_all_objects(offset=10).all()
+        self.assertEqual(len(instances), objective - 10)
+
+        instances = ViewModel.get_all_objects(limit=10).all()
+        self.assertEqual(len(instances), 10)
+
+    def test_get_one_object_by_name(self):
+        """
+        Test that the get_one_by_name method works properly
+        """
+        instance = ViewModel.get_one_by_name(name="instance")
+        self.assertEqual(instance.name, "instance")
+
+    def test_get_one_object(self):
+        """
+        Test that the get_one_object method works properly
+        """
+        instance = ViewModel.get_one_object(idx=1)
+        self.assertEqual(instance.name, "instance")
+
+    def test_get_one_object_without_idx(self):
+        """
+        Test that the get_one_object method works properly
+        """
+        instance = ViewModel.get_one_object(name="instance")
+        self.assertEqual(instance.name, "instance")
