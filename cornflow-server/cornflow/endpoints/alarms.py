83c5f62dcdffa93f9be4d02eeb0ae7b62358a723
Alejandra Galán <alejandra.galan@baobabsoluciones.es>
2025-03-11 17:26:09 +0100

    cleaning + roles

--- cornflow-server/cornflow/endpoints/alarms.py
+++ cornflow-server/cornflow/endpoints/alarms.py
@@ -13,6 +13,7 @@ from cornflow.schemas.alarms import (
 )
 from cornflow.shared.authentication import Auth, authenticate
 from cornflow.shared.exceptions import AirflowError, ObjectDoesNotExist, InvalidData
+from cornflow.shared.const import SERVICE_ROLE
 
 
 class AlarmsEndpoint(BaseMetaResource):
@@ -20,6 +21,8 @@ class AlarmsEndpoint(BaseMetaResource):
     Endpoint used to manage the table alarms.
     Available methods: [get, post]
     """
+
+    ROLES_WITH_ACCESS = [SERVICE_ROLE]
 
     def __init__(self):
         super().__init__()
@@ -62,53 +65,12 @@ class AlarmsEndpoint(BaseMetaResource):
         return self.post_list(data=kwargs)
 
 
-# class AlarmDetailEndpoint(BaseMetaResource):
-#     """
-#     Endpoint use to get the information of one single alarm
-#     """
-
-#     def __init__(self):
-#         super().__init__()
-#         self.data_model = AlarmsModel
-#         self.unique = ["id"]
-
-#     @doc(
-#         description="Get an alarm",
-#         tags=["None"],
-#     )
-#     @authenticate(auth_class=Auth())
-#     @marshal_with(AlarmsResponse(many=True))
-#     @BaseMetaResource.get_data_or_404
-#     def get(self, idx):
-#         """
-#         API method to get all the data of a specific alarm.
-#         It requires authentication to be passed in the form of a token that has to be linked to
-#         an existing session (login) made by a user.
-#         :return: The data of the alarm, and an integer with the HTTP status code.
-#         :rtype: Tuple(dict, integer)
-#         """
-
-#         current_app.logger.info(f"User {self.get_user()} gets details of alarm {idx}")
-
-#         return self.get_detail(idx=idx)
-
-#     @doc(description="Disable an alarm", tags=["None"])
-#     @authenticate(auth_class=Auth())
-#     def delete(self, idx):
-#         """
-#         :param int alarm_id: Alarm id.
-#         :return:
-#         :rtype: Tuple(dict, integer)
-#         """
-
-#         current_app.logger.info(f"Alarm {idx} was disabled by user {self.get_user()}")
-#         return self.disable_detail(idx=idx)
-
-
 class AlarmDetailEndpointBase(BaseMetaResource):
     """
     Endpoint used to get the information of a certain alarm. But not the data!
     """
+
+    ROLES_WITH_ACCESS = [SERVICE_ROLE]
 
     def __init__(self):
         super().__init__()
