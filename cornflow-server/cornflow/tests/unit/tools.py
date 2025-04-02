30fd809b460767aa9fa559d2d471b17eeb003f3b
Alejandra Galán <alejandra.galan@baobabsoluciones.es>
2025-04-02 19:26:12 +0200

    Solving test problems

--- cornflow-server/cornflow/tests/unit/tools.py
+++ cornflow-server/cornflow/tests/unit/tools.py
@@ -1,5 +1,9 @@
 from unittest.mock import Mock, patch
 from flask import Flask
+import json
+import os
+
+from cornflow.shared.const import DATABRICKS_TERMINATE_STATE, DATABRICKS_FINISH_TO_STATE_MAP
 
 
 def create_test_app():
@@ -41,8 +45,34 @@ def patch_db_client(db_client_class):
         responses_mock.json.return_value = {
             "status": {"state": "SUCCESS", "termination_details": {"code": "SUCCESS"}}
         }
+        response_run_workflow = Mock()
+        response_run_workflow.json.return_value = {
+            "run_id": 350148078719367,
+            "number_in_job": 350148078719367,
+        }
+        response_get_orch_info = Mock()
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        test_data_dir = os.path.join(os.path.dirname(current_dir), "data")
+        with open(
+            os.path.join(test_data_dir, "patch_databricks_get_orch_info.json")
+        ) as f:
+            response_get_orch_info.json.return_value = json.load(f)
+        response_get_run_status = Mock()
+        with open(
+            os.path.join(test_data_dir, "patch_databricks_get_run_status.json")
+        ) as f:
+            response_get_run_status.json.return_value = json.load(f)
+        state = response_get_run_status.json.return_value["status"]["state"]
+        if state == DATABRICKS_TERMINATE_STATE:
+            if (
+                    response_get_run_status.json.return_value["status"]["termination_details"]["code"]
+                    in DATABRICKS_FINISH_TO_STATE_MAP.keys()
+            ):
+                response_get_run_status.json.return_value = response_get_run_status.json.return_value["status"]["termination_details"]["code"]
+            else:
+                response_get_run_status.json.return_value = "OTHER_FINISH_ERROR"
         db_client_mock.is_alive.return_value = True
-        db_client_mock.get_orch_info.return_value = responses_mock
-        db_client_mock.run_workflow.return_value = responses_mock
-        db_client_mock.get_run_status.return_value = responses_mock
+        db_client_mock.get_orch_info.return_value = response_get_orch_info
+        db_client_mock.run_workflow.return_value = response_run_workflow
+        db_client_mock.get_run_status.return_value = response_get_run_status
         db_client_class.from_config.return_value = db_client_mock
