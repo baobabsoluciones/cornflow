a05d8b1a91ccc72dcaedd29460ff5909c9134d28
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2025-04-08 16:23:07 +0200

    Fixed error

--- cornflow-server/cornflow/commands/users.py
+++ cornflow-server/cornflow/commands/users.py
@@ -16,7 +16,7 @@ def create_user_with_role(
             current_app.logger.info(
                 f"User {username} is created and assigned {role_name} role"
             )
-            return
+        return
 
     user_roles = UserRoleModel.get_all_objects(user_id=user.id)
     user_actual_roles = [ur.role for ur in user_roles]
@@ -25,7 +25,7 @@ def create_user_with_role(
             current_app.logger.info(
                 f"User {username} exists and already has {role_name} role assigned"
             )
-            return
+        return
 
     user_role = UserRoleModel({"user_id": user.id, "role_id": role})
     user_role.save()
@@ -33,7 +33,7 @@ def create_user_with_role(
         current_app.logger.info(
             f"User {username} already exists and is assigned a {role_name} role"
         )
-        return
+    return
 
 
 def create_service_user_command(username, email, password, verbose: bool = True):
