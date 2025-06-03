6cb66406b5c81aa0b4e0603ebee7701b2f85b54d
Alejandra Galán <alejandra.galan@baobabsoluciones.es>
2025-06-03 11:53:37 +0200

    Endpoints get updated if any attribute changes

--- cornflow-server/cornflow/commands/views.py
+++ cornflow-server/cornflow/commands/views.py
@@ -1,4 +1,3 @@
-
 # Imports from external libraries
 from flask import current_app
 from importlib import import_module
@@ -8,25 +7,37 @@ import sys
 # Imports from internal libraries
 from cornflow.models import ViewModel
 from cornflow.shared import db
+from cornflow.endpoints import resources, alarms_resources
 
 
 def register_views_command(external_app: str = None, verbose: bool = False):
 
     if external_app is None:
-        from cornflow.endpoints import resources, alarms_resources
         resources_to_register = resources
         if current_app.config["ALARMS_ENDPOINTS"]:
             resources_to_register = resources + alarms_resources
     elif external_app is not None:
         sys.path.append("./")
         external_module = import_module(external_app)
-        resources_to_register = external_module.endpoints.resources
+        if current_app.config["ALARMS_ENDPOINTS"]:
+            resources_to_register = (
+                external_module.endpoints.resources + resources + alarms_resources
+            )
+        else:
+            resources_to_register = external_module.endpoints.resources + resources
     else:
         resources_to_register = []
         exit()
 
     views_registered = [view.name for view in ViewModel.get_all_objects()]
-
+    views_registered_urls_all_attributes = {
+        view.name: {
+            "url_rule": view.url_rule,
+            "description": view.description,
+            "id": view.id,
+        }
+        for view in ViewModel.get_all_objects()
+    }
     views_to_register = [
         ViewModel(
             {
@@ -38,6 +49,34 @@ def register_views_command(external_app: str = None, verbose: bool = False):
         for view in resources_to_register
         if view["endpoint"] not in views_registered
     ]
+
+    all_resources_to_register_views_endpoints = {
+        view["endpoint"]: {
+            "url_rule": view["urls"],
+            "description": view["resource"].DESCRIPTION,
+        }
+        for view in resources_to_register
+    }
+
+    # Check if views have the same name but different url_rule or description
+    for view_name, view_attrs in views_registered_urls_all_attributes.items():
+        if view_name in all_resources_to_register_views_endpoints.keys():
+            new_endpoint = all_resources_to_register_views_endpoints[view_name]
+            if (
+                view_attrs["url_rule"] != new_endpoint["url_rule"]
+                or view_attrs["description"] != new_endpoint["description"]
+            ):
+                # Update the object with new attributes
+                ViewModel.update(
+                    {
+                        "id": view_attrs["id"],
+                        "name": view_name,
+                        "url_rule": new_endpoint["url_rule"],
+                        "description": new_endpoint["description"],
+                    },
+                )
+        else:
+            ViewModel.delete(view_attrs["id"])
 
     if len(views_to_register) > 0:
         db.session.bulk_save_objects(views_to_register)
