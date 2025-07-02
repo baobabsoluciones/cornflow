5ceade739533a903c071ce117b25db05e00da108
Alejandra Galán <alejandra.galan@baobabsoluciones.es>
2025-07-02 16:18:56 +0200

    small change in logic for auth decorator

--- cornflow-server/cornflow/shared/authentication/decorators.py
+++ cornflow-server/cornflow/shared/authentication/decorators.py
@@ -39,28 +39,35 @@ def authenticate(
             :param kwargs: the original kwargs sent to the decorated function
             :return: the result of the call to the function
             """
-            if optional_auth is None:
-                # Default behavior: always require authentication
-                if auth_class.authenticate():
-                    return func(*args, **kwargs)
-                else:
-                    raise InvalidCredentials("Unable to authenticate the user")
-            else:
-                if no_auth_list is None:
-                    raise InvalidCredentials(
-                        "The list of the values that indicates if authentication should be deactivated is not defined"
-                    )
-                env_variable = current_app.config[optional_auth]
-                if env_variable in no_auth_list:
-                    # Authentication is deactivated for this value
-                    return func(*args, **kwargs)
-                else:
-                    # Authentication is required
-                    if auth_class.authenticate():
-                        return func(*args, **kwargs)
-                    else:
-                        raise InvalidCredentials("Unable to authenticate the user")
+            if endpoint_qualified_for_no_auth(no_auth_list, optional_auth):
+                # Authentication is deactivated for this value
+                return func(*args, **kwargs)
 
+            if auth_class.authenticate():
+                return func(*args, **kwargs)
+            else:
+                raise InvalidCredentials("Unable to authenticate the user")
         return wrapper
 
     return decorator
+
+def endpoint_qualified_for_no_auth(no_auth_list, optional_auth):
+    """
+    This function is used to check if the endpoint is qualified for no authentication.
+
+    :param no_auth_list: the list of the values that indicates if authentication should be deactivated
+    :param optional_auth: the env variable that indicates if authentication should be deactivated
+    :type no_auth_list: list
+    :type optional_auth: str
+    :return: True if the endpoint is qualified for no authentication, False otherwise
+    """
+    if optional_auth is None:
+        return False
+    if no_auth_list is None:
+        raise InvalidCredentials(
+            "The list of the values that indicates if authentication should be deactivated is not defined"
+        )
+    env_variable = current_app.config[optional_auth]
+    if env_variable in no_auth_list:
+        return True
+    return False
