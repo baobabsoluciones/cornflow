be03983d538accd63b79cecdfac8bd77f9830abf
Alejandra Galán <alejandra.galan@baobabsoluciones.es>
2025-11-04 16:36:00 +0100

    bump version

--- cornflow-server/cornflow/shared/const.py
+++ cornflow-server/cornflow/shared/const.py
@@ -7,7 +7,7 @@ AIRFLOW_BACKEND = 1
 DATABRICKS_BACKEND = 2
 
 
-CORNFLOW_VERSION = "1.3.0rc6"
+CORNFLOW_VERSION = "1.3.0rc7"
 INTERNAL_TOKEN_ISSUER = "cornflow"
 
 # endpoints responses for health check
