9176ae26ded7f647d21c904d25a4ba34cab3f820
Alejandra Galán <alejandra.galan@baobabsoluciones.es>
2026-08-03 14:01:11 +0200

    bump cornflow

--- cornflow-server/cornflow/shared/const.py
+++ cornflow-server/cornflow/shared/const.py
@@ -7,7 +7,7 @@ AIRFLOW_BACKEND = 1
 DATABRICKS_BACKEND = 2
 
 
-CORNFLOW_VERSION = "1.3.7rc1"
+CORNFLOW_VERSION = "1.3.9rc1"
 INTERNAL_TOKEN_ISSUER = "cornflow"
 
 # ---------------------------------------------------------------------------
